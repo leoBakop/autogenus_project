4314594c4958b399bd95de3e6f74042c675193c6
leoBakop <you@example.com>
2022-02-20 16:59:59 +0200

    first commit

---
+++ FieldPlayer.java
@@ -0,0 +1,292 @@
+//-----------------------------------------------------------------------------
+//  File:         FieldPlayer.java (to be used in a Webots java controllers)
+//  Date:         April 30, 2008
+//  Description:  Field player "2", "3" or "4" for "red" or "blue" team
+//  Project:      Robotstadium, the online robot soccer competition
+//  Author:       Yvan Bourquin - www.cyberbotics.com
+//  Changes:      November 4, 2008: Adapted to Webots6
+//-----------------------------------------------------------------------------
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import com.cyberbotics.webots.controller.*;
+
+public class FieldPlayer extends Player {
+
+  private Motion backwardsMotion, forwardsMotion, forwards50Motion, turnRight40Motion, turnLeft40Motion;
+  private Motion turnRight60Motion, turnLeft60Motion, turnLeft180Motion, sideStepRightMotion, sideStepLeftMotion;
+  private Motion shooting;
+  private double goalDir = 0.0; // interpolated goal direction (with respect to front direction of robot body)
+  private boolean shortDist; //new
+
+
+  public FieldPlayer(int playerID, int teamID) {
+    super(playerID, teamID);
+    backwardsMotion     = new Motion("../../motions/Backwards.motion");
+    forwardsMotion      = new Motion("../../motions/Forwards.motion");
+    forwards50Motion    = new Motion("../../motions/Forwards50.motion");
+    turnRight40Motion   = new Motion("../../motions/TurnRight40.motion");
+    turnLeft40Motion    = new Motion("../../motions/TurnLeft40.motion");
+    turnRight60Motion   = new Motion("../../motions/TurnRight60.motion");
+    turnLeft60Motion    = new Motion("../../motions/TurnLeft60.motion");
+    turnLeft180Motion   = new Motion("../../motions/TurnLeft180.motion");
+    sideStepRightMotion = new Motion("../../motions/SideStepRight.motion");
+    sideStepLeftMotion  = new Motion("../../motions/SideStepLeft.motion");
+    shooting= new Motion ("../../motions/Shoot.motion");  // new code
+    shortDist=false;
+
+    // move arms along the body
+    Motor leftShoulderPitch = getMotor("LShoulderPitch");
+    leftShoulderPitch.setPosition(1.5);
+    Motor rightShoulderPitch = getMotor("RShoulderPitch");
+    rightShoulderPitch.setPosition(1.5);
+  }
+
+  // normalize angle between -PI and +PI
+  private double normalizeAngle(double angle) {
+    while (angle > Math.PI) angle -= 2.0 * Math.PI;
+    while (angle < -Math.PI) angle += 2.0 * Math.PI;
+    return angle;
+  }
+
+  // relative body turn
+  private void turnBodyRel(double angle) {
+    if (angle > 0.7)
+      turnRight60();
+    else if (angle < -0.7)
+      turnLeft60();
+    else if (angle > 0.3)
+      turnRight40();
+    else if (angle < -0.3)
+      turnLeft40();
+  }
+
+  protected void runStep() {
+    super.runStep();
+    double dir = camera.getGoalDirectionAngle();
+    if (dir != NaoCam.UNKNOWN)
+      goalDir = dir - headYawPosition.getValue();
+  }
+
+  private void turnRight60() {
+    playMotion(turnRight60Motion); // 59.2 degrees
+    goalDir = normalizeAngle(goalDir - 1.033);
+  }
+
+  private void turnLeft60() {
+    playMotion(turnLeft60Motion); // 59.2 degrees
+    goalDir = normalizeAngle(goalDir + 1.033);
+  }
+
+  private void turnRight40() {
+    playMotion(turnRight40Motion); // 39.7 degrees
+    goalDir = normalizeAngle(goalDir - 0.693);
+  }
+
+  private void turnLeft40() {
+    playMotion(turnLeft40Motion); // 39.7 degrees
+    goalDir = normalizeAngle(goalDir + 0.693);
+  }
+  
+  private void turnLeft180() {
+    playMotion(turnLeft180Motion); // 163.6 degrees
+    goalDir = normalizeAngle(goalDir + 2.855);
+  }
+
+  //method that perform the same moves as above
+  //but without tracking the ball 
+  // in order to keep the top camera turned on
+  // in order to find teammates
+
+
+  private void tTurnLeft60(){
+    tPlayMotion(turnLeft60Motion);
+    goalDir = normalizeAngle(goalDir - 1.033);
+  }
+
+  @Override public void run() {
+    boolean alreadySearch=true;
+    step(SIMULATION_STEP);
+
+    while (true) {
+      alreadySearch=!alreadySearch;
+      runStep();
+
+      getUpIfNecessary();
+
+      
+      int i = 1;
+
+      while (getBallDirection() == NaoCam.UNKNOWN) {
+        if(i%3==0){//if you have already searched for the ball three times, take a step back.
+          playMotion(backwardsMotion);
+          playMotion(backwardsMotion);
+          System.out.println(i);
+          i++;
+        }
+        if(shortDist){
+          playMotion(backwardsMotion);
+          playMotion(backwardsMotion);
+          shortDist=false;
+        }else{
+        System.out.println("searching the ball"); 
+        getUpIfNecessary();
+        if (getBallDirection() != NaoCam.UNKNOWN) break;
+        System.out.println("performing head scan");
+        headScan();
+        if (getBallDirection() != NaoCam.UNKNOWN) break;
+        playMotion(backwardsMotion);
+        if (getBallDirection() != NaoCam.UNKNOWN) break;
+        headScan();
+        if (getBallDirection() != NaoCam.UNKNOWN) break;
+        turnLeft180();
+        }
+        
+      }
+
+      double ballDir = getBallDirection();
+      double ballDist = getBallDistance();
+
+      
+
+
+      System.out.println("ball dist: " + ballDist + " ball dir: " + ballDir + " goal dir: " + goalDir);
+
+      if (ballDist < 0.3) {
+      LinkedList <Boolean> teammatePosition = new LinkedList<Boolean>(); //left = true and right is false
+       // if(!alreadySearch){
+          for(int j=0; i<7; i++){
+            System.out.println("searching for teammate");
+            if(camera.searchTeammate()){
+              if(i<3) teammatePosition.addLast(true);
+              else  teammatePosition.addLast(false);
+            } 
+            
+            tTurnLeft60();
+            
+          }
+          LinkedList<Double> teammateDir =getTeammateDirection();
+          LinkedList<Double> teammateDist = getTeammateDistance();
+          camera.selectBottom();
+          if(teammateDir.isEmpty()) System.out.println("terastio error gtxm");
+      //}
+        //current section testing
+        //find the best tactic for the current state(pass or shoot)
+        LinkedList bestTeammate=bestTactic(teammateDir, teammatePosition);
+        double bestTeammateDir=(double) bestTeammate.getFirst();
+        boolean isTeammateLeft=(boolean) bestTeammate.getLast();
+        if(isTeammateLeft) System.out.println("best teammate is on the left"); 
+        else System.out.println("best teammate is on the right");
+        //testing passing session
+        passing(isTeammateLeft, bestTeammateDir, ballDist);
+        //end of current section testing
+
+
+        System.out.println("short distance");
+        shortDist=true;
+        if (ballDir < -0.15){    //sometimes there is a deadlock between 138 and 142 line
+          playMotion(sideStepLeftMotion);
+          System.out.println("lagou step left");
+        }
+        else if (ballDir > 0.15 && Math.random()<0.85){ // we enter this if with 75% propability
+          playMotion(sideStepRightMotion);
+          System.out.println("lagou step right");
+        }
+        else if (goalDir < -0.35)
+          turnLeft40();
+        else if (goalDir > 0.35)
+          turnRight40();
+        else {
+          System.out.println("try to shoot");
+          playMotion(sideStepRightMotion);// my robot is a left footed player so in order to shoot the ball with the left foot
+          System.out.println("took a small right step");// it performs a right step
+          if(ballDist>0.17)
+            playMotion(forwardsMotion); //take a small forward step
+          System.out.println("shooting !!!");
+          playMotion(shooting);
+        }
+      }
+      else {
+        shortDist=false;//new code
+        
+        double goDir = normalizeAngle(ballDir - goalDir);
+
+        if (goDir < ballDir - 0.5)
+          goDir = ballDir - 0.5;
+        else if (goDir > ballDir + 0.5)
+          goDir = ballDir + 0.5;
+
+        goDir = normalizeAngle(goDir);
+
+        turnBodyRel(goDir);
+        if (ballDist < 0.6)
+          playMotion(forwardsMotion);
+        else
+          playMotion(forwards50Motion);
+      }
+    }
+  }
+
+  private void passing(boolean isLeft, double dir, double ballDist){
+    System.out.println("trying to pass");
+   
+    if(isLeft){//teammate is on the left
+      System.out.println("teammate is on the left");
+      turnBodyRel(dir);
+      playMotion(backwardsMotion);
+      playMotion(backwardsMotion);
+      for(int i=0; i<5; i++){
+        playMotion(sideStepRightMotion);
+      }
+
+      playMotion(forwardsMotion);
+      playMotion(sideStepRightMotion);
+      for(int j=0; j<3; j++)
+        playMotion(forwardsMotion);
+      
+      turnBodyRel(dir);
+      playMotion(shooting);
+    }else if(!isLeft){ //teammate is on the right
+
+      System.out.println("teammate is on the right");
+      playMotion(sideStepLeftMotion);
+      playMotion(sideStepLeftMotion);
+      turnRight60();
+      playMotion(backwardsMotion);
+      playMotion(backwardsMotion);
+      for(int i=0; i<4; i++){
+        playMotion(sideStepLeftMotion);
+      }
+      playMotion(forwardsMotion);
+      playMotion(sideStepRightMotion);
+      System.out.println("outside right if");
+      
+      turnBodyRel(-dir);
+      if(Math.abs(dir)<0.3){
+        playMotion(forwardsMotion);
+        playMotion(sideStepRightMotion);
+      }else if(Math.abs(dir)>0.3){
+        playMotion(sideStepLeftMotion);  
+      }
+      else if(Math.abs(dir)>0.5){
+        playMotion(sideStepLeftMotion);
+        playMotion(sideStepLeftMotion);
+      }
+
+      playMotion(shooting);
+
+    }
+  }
+
+  private LinkedList bestTactic(LinkedList<Double> teammates, LinkedList<Boolean> position){
+    LinkedList retVal= new LinkedList();
+    if(teammates.isEmpty()) return null;
+    retVal.addLast(teammates.getFirst());
+    retVal.addLast(position.getFirst());
+    
+
+    System.out.println(teammates.getLast());
+    return retVal;
+  }
+}
---
+++ NaoCam.java
@@ -0,0 +1,183 @@
+//-----------------------------------------------------------------------------
+//  File:         NaoCam.java (to be used in a Webots java controllers)
+//  Date:         April 30, 2008
+//  Description:  Camera with simple blob localization capability
+//  Project:      Robotstadium, the online robot soccer competition
+//  Author:       Yvan Bourquin - www.cyberbotics.com
+//  Changes:      November 4, 2008: Adapted to Webots6
+//-----------------------------------------------------------------------------
+
+import java.lang.Math;
+import java.util.LinkedList;
+
+import com.cyberbotics.webots.controller.Camera;
+import com.cyberbotics.webots.controller.Robot;
+
+public class NaoCam {
+
+  public static final double UNKNOWN = -999.9;
+  public static final double OFFSET_ANGLE = 0.6981;  // 40 degrees between cameras axes
+  public enum Goal { SKY_BLUE, YELLOW, UNKNOWN_COLOR };
+
+  boolean topSelected = true;
+  Camera topCamera;
+  Camera bottomCamera;
+  int timeStep;
+  private Goal goalColor;
+  private double fov;
+  private int width, height;
+  private int[] image;
+  private double blobDirectionAngle = UNKNOWN;
+  private double blobElevationAngle = UNKNOWN;
+  private LinkedList<Double> tDirAngle;
+  private LinkedList<Double> tElevationAngle;
+  private double ballDirectionAngle = UNKNOWN;
+  private double ballElevationAngle = UNKNOWN;
+  private double goalDirectionAngle = UNKNOWN;
+
+  //new code
+  private boolean isRed;
+
+  public NaoCam(Player robot, int timeStep) {  //change from Robot to Player
+    topCamera = robot.getCamera("CameraTop");
+    bottomCamera = robot.getCamera("CameraBottom");
+    
+    tDirAngle=new LinkedList<Double>();
+    tElevationAngle=new LinkedList<Double>();
+
+    this.timeStep = timeStep;
+    goalColor = Goal.UNKNOWN_COLOR;
+    isRed=robot.isRed();
+    // start with top camera
+    selectTop();
+  }
+  
+  public void setGoalColor(Goal goal) {
+    this.goalColor = goal;
+  }
+
+  // find a blob whose rgb components match
+  private void findColorBlob(int R, int G, int B, int threshold) {
+
+    int x = 0, y = 0;
+    int npixels = 0;
+
+    for (int i = 0; i < image.length; i++) {
+      int r = Camera.pixelGetRed(image[i]);
+      int g = Camera.pixelGetGreen(image[i]);
+      int b = Camera.pixelGetBlue(image[i]);
+
+      if (Math.abs(r - R) + Math.abs(g - G) + Math.abs(b - B) < threshold) {
+        x += i % width;
+        y += i / width;
+        npixels++;
+      }
+    }
+
+    if (npixels > 0) {
+      blobDirectionAngle = ((double)x / npixels / width - 0.5) * fov;
+      blobElevationAngle = -((double)y / npixels / height - 0.5) * fov;
+    }
+    else {
+      blobDirectionAngle = UNKNOWN;
+      blobElevationAngle = UNKNOWN;
+    }
+  }
+
+  // analyse image and find blobs
+  public void processImage() {
+
+    if (topSelected)
+      image = topCamera.getImage();
+    else
+      image = bottomCamera.getImage();
+    
+    // find orange ball
+    findColorBlob(240, 140, 50, 60);
+    ballDirectionAngle = blobDirectionAngle;
+    ballElevationAngle = blobElevationAngle;
+
+    
+    
+
+    // find goal
+    switch (goalColor) {
+    case SKY_BLUE:
+      findColorBlob(30, 200, 200, 60);
+      break;
+    case YELLOW:
+      findColorBlob(140, 140, 15, 60);
+      break;
+    default:
+      goalDirectionAngle = UNKNOWN;
+    }
+
+    goalDirectionAngle = blobDirectionAngle;
+    //System.out.println("camera: goal: dir: " + goalDirectionAngle);
+  }
+
+
+  // i have to find a way to select top camera
+  public boolean searchTeammate(){
+    //new code
+    //find teammate
+    selectTop();
+    
+    if(isRed) findColorBlob(0,0,0, 60);
+    else findColorBlob(51, 57, 240, 60);  
+    
+    tDirAngle.addLast( blobDirectionAngle);
+    tElevationAngle.addLast(blobElevationAngle);
+    return ! tDirAngle.isEmpty();
+    
+  }
+
+  // all direction and elevation angles are indicated in radians
+  // with respect to the camera focal (or normal) line
+  // a direction angle will not exceed +/- half the field of view
+  // a positive direction is towards the right of the camera image
+  // a positive elevation is towards the top of the camera image
+  public double getBallDirectionAngle() {
+    return ballDirectionAngle;
+  }
+
+  public double getBallElevationAngle() {
+    return ballElevationAngle;
+  }
+
+  public double getGoalDirectionAngle() {
+    return goalDirectionAngle;
+  }
+
+  public LinkedList<Double> getTeammateDirAngle(){
+    return tDirAngle;
+  }
+  public LinkedList<Double> getTeammateElevationAngle(){
+    return tElevationAngle;
+  }
+  
+  public void selectTop() {
+    bottomCamera.disable(); 
+    topCamera.enable(timeStep);
+    fov = topCamera.getFov();
+    width = topCamera.getWidth();
+    height = topCamera.getHeight();
+    topSelected = true;
+  }
+
+  public void selectBottom() {
+    topCamera.disable(); 
+    bottomCamera.enable(timeStep);
+    fov = bottomCamera.getFov();
+    width = bottomCamera.getWidth();
+    height = bottomCamera.getHeight();
+    topSelected = false;
+  }
+  
+  public double getOffsetAngle() {
+    if (topSelected)
+      return 0.0;
+    else
+      return OFFSET_ANGLE;
+  }
+}
